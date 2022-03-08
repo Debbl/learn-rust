86c88fb11a378426eb6715765af8d11ae02a80dd
Debbl <me@aiwan.run>
2022-03-08 20:58:47 +0800

    循环和match

--- 02_guessing_game/src/main.rs
+++ 02_guessing_game/src/main.rs
@@ -1,22 +1,30 @@
-use std::io;
 use rand::Rng; // trait
 use std::cmp::Ordering;
+use std::io;
 
 fn main() {
     println!("猜数！");
 
     let secret_number = rand::thread_rng().gen_range(1..101);
     println!("神秘数字是: {}", secret_number);
-    println!("猜测一个数");
 
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess).expect("无法读取行");
-    println!("你猜测的数是：{}", guess);
+    loop {
+        println!("猜测一个数");
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("无法读取行");
+        println!("你猜测的数是：{}", guess);
 
-    let guess: u32 = guess.trim().parse().expect("Please type a number");
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small"),
-        Ordering::Greater => println!("Too big"),
-        Ordering::Equal => println!("You win!"),
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 }
