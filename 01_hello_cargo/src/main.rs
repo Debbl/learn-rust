8553032ddb25f53c4baec717a1da345aa17c73c3
Debbl <me@aiwan.run>
2022-10-26 17:34:45 +0800

    chore: update

--- 01_hello_cargo/src/main.rs
+++ 01_hello_cargo/src/main.rs
@@ -1,3 +1,12 @@
+fn greet_world() {
+    let southern_germany = "Grüß Gott!";
+    let chinese = "世界，你好";
+    let english = "Hello, world!";
+    let regions = [southern_germany, chinese, english];
+    for r in regions.iter() {
+        println!("{}", &r);
+    }
+}
 fn main() {
-    println!("Hello, world!");
+    greet_world();
 }
