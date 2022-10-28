bc5706cd868567f03824337fe0127c1e92f6b1bd
Debbl <me@aiwan.run>
2022-10-28 19:29:45 +0800

    chore: functions

--- 04_functions/src/main.rs
+++ 04_functions/src/main.rs
@@ -1,5 +1,5 @@
 // # functions
-/* 
+/*
 fn main() {
     println!("Hello, world!");
 
@@ -13,11 +13,53 @@ fn another_function() {
 
 // ** parameters
 
-fn main() {
+/* fn main() {
     println!("Hello world!");
     another_function(5);
 }
 
 fn another_function(x: i32) {
-    println!("The value of x is: {}", x);
+    println!("The value of x is: {x}");
+} */
+
+/* fn main() {
+    print_labeled_measurement(5, 'h')
+}
+
+fn print_labeled_measurement(value: i32, unit_label: char) {
+    println!("The measurement is: {value}{unit_label}");
+} */
+
+/* fn main() {
+    // let y = 6;
+    // let x = (let y = 6); // error
+
+} */
+
+// statements and expressions
+/* fn main() {
+    let y = {
+        let x = 3;
+        x + 1
+    };
+    println!("The value of y is: {y}");
+} */
+
+/* fn main() {
+    let x = five();
+    println!("The value of x is: {x}");
+}
+
+fn five() -> i32 {
+    5
+} */
+
+fn main() {
+    let x = plus_one(5);
+    println!("The value of x is: {x}");
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
+    // x + 1; // error
 }
